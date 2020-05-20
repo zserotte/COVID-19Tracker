6e585250976e6663f65c933fb5230a0da7c204ff
Zachary Cook Serotte <serotte.z@wustl.edu>
2020-05-20 17:33:30 -0400

    Create Globals.java

---
+++ Corona/src/Globals.java
@@ -0,0 +1,22 @@
+package src;
+public class Globals {
+	public static String region;
+	public static String country_total_cases;
+	public static String country_total_deaths;
+	public static String country_new_cases;
+	public static String country_active_cases;
+	public static String country_new_deaths;
+	public static String country_total_recovered;
+	
+	public static String province; 
+	public static int province_confirmed;
+	public static int province_deaths;
+	public static String resident_country;
+	public static String country_visited;
+	public static int age;
+	public static int weight;
+	public static int height; 
+	
+	public static boolean extractedCountryInfo;
+	public static Province[] provinceArray = new Province[2881];
+}
